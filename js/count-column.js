8a003270c544d74dec48d21fbc6f1fe2ea8e8d1c
Choi <wldnd12223@gmail.com>
2023-10-02 23:27:42 +0900

    first commit

--- js/count-column.js
+++ js/count-column.js
@@ -40,19 +40,19 @@ $(function() {
     
     }, 1000);
   
-      // function upslide() {
-      //     var h = $('#ticker').find('tbody > tr').outerHeight(); 
+      function upslide() {
+          var h = $('#ticker').find('tbody > tr').outerHeight(); 
           
-      //     var item = $('#ticker').find('tbody > tr:first-child'); 
-      //     var itemClone = item.clone(); 
-      //     $('#ticker > tbody').append(itemClone);
+          var item = $('#ticker').find('tbody > tr:first-child'); 
+          var itemClone = item.clone(); 
+          $('#ticker > tbody').append(itemClone);
           
-      //     $('#ticker_wrap').css({'top':0}).animate({'top': '-' + h +'px'}, function(){ 
-      //         $('#ticker_wrap').css({'top': 0}); 
-      //         item.remove(); 
-      //     }); 
-      // }
-      // setInterval(function(){upslide()}, 1700);
+          $('#ticker_wrap').css({'top':0}).animate({'top': '-' + h +'px'}, function(){ 
+              $('#ticker_wrap').css({'top': 0}); 
+              item.remove(); 
+          }); 
+      }
+      setInterval(function(){upslide()}, 1700);
     
     })
   
--- js/menu.js
+++ js/menu.js
@@ -17,49 +17,27 @@ menu.innerHTML = `
 
       <div class="logo float-left">
         <!-- Uncomment below if you prefer to use an image logo -->
-        <h1 class="text-light"><a href="../index.html" ><span>FLOS</span></a></h1>
+        <h1 class="text-light"><a href="../index.html" ><span>호관원 프리미엄</span></a></h1>
         <!-- <a href="#header" class="scrollto"><img src="./intro/img/logo.png" alt="" class="img-fluid"></a> -->
       </div>
 
       <nav class="main-nav float-right d-none d-lg-block">
         <ul>
           <li ><a class="list-btn" href="../index.html">홈</a></li>
-         
-          <li class="drop-down"><a class="list-btn" href="">회사 소개</a>
+          <li ><a class="list-btn" href="../about.html">회사 소개</a></li>
+          <li ><a class="list-btn" href="../columns.html">관절 칼럼</a></li>
+         <!-- <li class="drop-down"><a class="list-btn" href="">회사 소개</a>
             <ul>
             <li><a class="list-btn menu-about" href="../about.html">FLOS는?</a></li>
             <li><a class="list-btn menu-motive" href="../motive.html">사업을 시작 이유?</a></li>
-              <!-- <li class="drop-down"><a href="#">Drop Down 2</a>
-                <ul>
-                  <li><a class="list-btn" href="#">Deep Drop Down 1</a></li>
-                  <li><a class="list-btn" href="#">Deep Drop Down 2</a></li>
-                  <li><a class="list-btn" href="#">Deep Drop Down 3</a></li>
-                  <li><a class="list-btn" href="#">Deep Drop Down 4</a></li>
-                  <li><a class="list-btn" href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a class="list-btn" href="#">Drop Down 3</a></li>
-              <li><a class="list-btn" href="#">Drop Down 4</a></li>
-              <li><a class="list-btn" href="#">Drop Down 5</a></li> -->
             </ul>
-          </li>
-          <li class="drop-down"><a class="list-btn menu-column" href="../column/column-event.html">무료 칼럼</a>
+          </li> -->
+          <!-- <li class="drop-down"><a class="list-btn menu-column" href="../column/column-event.html">무료 칼럼</a>
             <ul>
               <li><a class="list-btn" href="../column/column-event.html">고객을 확보하는 홈페이지 제작법</a></li>
-              <!-- <li class="drop-down"><a href="#">Drop Down 2</a>
-                <ul>
-                  <li><a class="list-btn" href="#">Deep Drop Down 1</a></li>
-                  <li><a class="list-btn" href="#">Deep Drop Down 2</a></li>
-                  <li><a class="list-btn" href="#">Deep Drop Down 3</a></li>
-                  <li><a class="list-btn" href="#">Deep Drop Down 4</a></li>
-                  <li><a class="list-btn" href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a class="list-btn" href="#">Drop Down 3</a></li>
-              <li><a class="list-btn" href="#">Drop Down 4</a></li>
-              <li><a class="list-btn" href="#">Drop Down 5</a></li> -->
+              
             </ul>
-          </li>
+          </li> -->
          <!-- <li class="drop-down"><a class="list-btn" href="../program.html">제작 과정</a>
           <ul>
             <li><a class="list-btn" href="../column/칼럼1.html">1. 홈페이지 기획</a></li>
@@ -80,10 +58,10 @@ menu.innerHTML = `
             <li><a class="list-btn" href="#">Drop Down 5</a></li> 
           </ul>
         </li> -->
-          <li><a class="list-btn menu-service" href="../service.html">제공 서비스</a></li>
+          <li><a class="list-btn menu-service" href="../effect.html">효능 소개</a></li>
           <li><a class="list-btn menu-faq" href="../faq.html">FAQ</a></li>
           
-          <li ><a class="imp-btn" href="../column/consulting.html">문의 신청하기</a></li>
+          <li ><a class="imp-btn" href="../column/consulting.html">무료 상담 신청</a></li>
         </ul>
       </nav><!-- .main-nav -->
       
